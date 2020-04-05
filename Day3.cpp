f8a2316fe3dbc3bd863e7c5aef3723d3055c7c19
Vlad Kutuzov <48442774+qqullar@users.noreply.github.com>
2020-04-06 00:31:31 +0300

    Day3 challenge

---
+++ Day3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int maxSumOfSubArr(std::vector<int> &v) {
+	
+	size_t n = v.size();
+	
+	int localMax = 0;
+	int globalMax = INT_MIN;
+	
+	for(size_t i = 0; i < n; ++i) {
+		localMax = std::max(v[i], v[i]+localMax);
+		
+		if(localMax > globalMax) {
+			globalMax = localMax;
+		}
+		
+	}
+	
+	return globalMax;
+}
+
+void test() {
+	std::vector<int> v = { -2, 1, -3, 4, -1 , 2, 1, -5, 4 };
+	
+	std::cout << maxSumOfSubArr(v) << std::endl;
+}
+
+int main() {
+	test();
+}
